f65811a1308bb771f4fc0676776bed1d1e807808
Alireza Ghadimi <alirezaghadimy@gmail.com>
2017-05-29 23:14:58 +0430

    Write first example

---
+++ examples/1.js
@@ -0,0 +1,32 @@
+/*****************************************************************************
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 3 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *___________________________________________________________________________*
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *___________________________________________________________________________*
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *___________________________________________________________________________*
+ *                             Created by  Qti3e                             *
+ *        <http://Qti3e.Github.io>    LO-VE    <Qti3eQti3e@Gmail.com>        *
+ *****************************************************************************/
+
+const redis_search_lib  = require('../src');
+
+const redis_search  = new redis_search_lib(3);
+const content       = 'Lorem ipsum dolor sit amet, eu ius iusto populo, affert honestatis est ne. Ne debet perfecto oportere sea, ei sed labitur laboramus. Te ludus civibus eam, no regione similique eum. Persecuti persequeris ea sea, ius vidisse elaboraret ea.';
+redis_search.index(content, 5, 2);
+setTimeout(function () {
+	redis_search.search('Lorip', (id, count) => {
+		console.log(id, count);
+		// We count pages from 0
+		redis_search.results(id, 0, data => {
+			console.log(data)
+		})
+	});
+}, 1000);
